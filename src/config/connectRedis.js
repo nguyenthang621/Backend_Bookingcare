bcc6280de9978a44d0f7572ddcec8328911406a7
nguyenthang621 <thang.nd196447@sis.hust.edu.vn>
2023-08-10 13:31:37 +0700

    fix redis

--- src/config/connectRedis.js
+++ src/config/connectRedis.js
@@ -4,29 +4,29 @@ import { createClient } from 'redis';
 // const client = createClient({
 //     URL: 'redis-18099.c257.us-east-1-3.ec2.cloud.redislabs.com:18099',
 // });
-// const client = createClient({
-//     port: process.env.REDIS_PORT,
-//     host: process.env.REDIS_HOST,
-//     username: process.env.REDIS_USERNAME,
-//     password: process.env.REDIS_PASSWORD,
-//     url: process.env.REDIS_URL,
-//     //------------------------------
-//     // password: process.env.REDIS_PASSWORD,
-//     // socket: {
-//     //     host: process.env.REDIS_HOST,
-//     //     port: process.env.REDIS_PORT,
-//     // },
-
-//     //-------------------------------
-//     // url: process.env.REDIS_URL,
-// });
 const client = createClient({
-    password: 'rO34rRMhVcc8iQseqOWRe84TFDN1rpit',
-    socket: {
-        host: 'redis-18099.c257.us-east-1-3.ec2.cloud.redislabs.com',
-        port: 18099,
-    },
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    url: process.env.REDIS_URL,
+    //------------------------------
+    // password: process.env.REDIS_PASSWORD,
+    // socket: {
+    //     host: process.env.REDIS_HOST,
+    //     port: process.env.REDIS_PORT,
+    // },
+
+    //-------------------------------
+    // url: process.env.REDIS_URL,
 });
+// const client = createClient({
+//     password: 'rO34rRMhVcc8iQseqOWRe84TFDN1rpit',
+//     socket: {
+//         host: 'redis-18099.c257.us-east-1-3.ec2.cloud.redislabs.com',
+//         port: 18099,
+//     },
+// });
 client.ping((err, pong) => {
     console.log('ping: ', pong);
 });
